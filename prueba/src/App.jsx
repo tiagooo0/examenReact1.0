c1473cf0c7a746d8cde4621856fa271bc517d6cc
tiagooo0 <thurstaltamirano@escuelasproa.edu.ar>
2024-08-26 10:46:28 -0300

    saco readme y cambios

--- prueba/src/App.jsx
+++ prueba/src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 //hago mis imports
 import Calculadora from './components/Calculadora'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
